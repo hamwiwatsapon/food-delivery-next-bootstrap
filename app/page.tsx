47de1508bfe31ebf14e6c70ffa92421618542a3b
Wiwatsapon <hamlert33@gmail.com>
2024-03-12 11:45:48 +0700

    change font

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
-import styles from './page.module.css'
-import { Poppins, Montserrat } from 'next/font/google'
-
-const poppins = Poppins({
-  weight: ['300','400','500','600','700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-poppins',
-})
-
-const montserrat = Montserrat({
-  weight: ['300','400','500','600','700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-})
+import styles from "./page.module.css"
 
 const Home = () => {
   return (
-    <main className={`${poppins.className},${montserrat.className},${styles.main}`}>
-      TEST
+    <main className={styles.main}>
+      <h1>TEST</h1>
     </main>
   )
 }
